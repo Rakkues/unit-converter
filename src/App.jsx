b7ba7ccc2fd43f752969511b5f48379bdca2c499
Anas <anaska1311@gmail.com>
2026-04-01 17:38:12 +0800

    Added the convert buttons to the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
 import "./App.css";
 
-function App() {}
+const lengthUnits = ["centimenter", "inch", "meter"];
+
+function App() {
+  return (
+    <>
+      <h1>Unit Converter</h1>
+      <UnitCoverter unitType="length" />
+    </>
+  );
+}
+
+function UnitCoverter(props) {
+  return (
+    <form id={`${props.unitType}ConvertForm`} className="convertForm">
+      <label for={`from${props.unitType}`}>Enter the length to convert</label>
+      <input type="text" id="fromLength" required />
+      <label for="fromUnit">Unit to convert from</label>
+      <select name="fromUnit" id="fromUnit" required>
+        {lengthUnits.map((length) => {
+          return <option value={length}>{length}</option>;
+        })}
+      </select>
+      <label for="toUnit">Unit to convert to</label>
+      <select name="toUnit" id="toUnit" required>
+        {lengthUnits.map((length) => {
+          return <option value={length}>{length}</option>;
+        })}
+      </select>
+      <input type="submit" value="Convert" className="submitBtn" />
+    </form>
+  );
+}
 
 export default App;
